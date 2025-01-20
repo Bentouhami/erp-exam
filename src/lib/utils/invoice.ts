e1393218e4e8c263eda7abfa14102786be1d26ed
Faisal Bentouhami <bentouhami.faisal@gmail.com>
2025-01-20 18:58:19 +0100

    fixe bug on vercel deployed app

--- src/lib/utils/invoice.ts
+++ src/lib/utils/invoice.ts
@@ -1,36 +1,32 @@
 // path: src/lib/utils/invoice.ts
 
 import prisma from "@/lib/db";
-export async function generateInvoiceNumber(): Promise<string> {
 
-    // Get the current year and month
+export async function generateInvoiceNumber(): Promise<string> {
     const date = new Date();
-    const year = date.getFullYear().toString().slice(-2); // Get last 2 digits of year
+    const year = date.getFullYear().toString().slice(-2);
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
 
-    // Rechercher la dernière facture émise pour l'année en cours
-    const lastInvoice = await prisma.invoice.findFirst({
-        where: {
-            invoiceNumber: {
-                startsWith: `INV${year}${month}`
+    return await prisma.$transaction(async (tx) => {
+        const lastInvoice = await tx.invoice.findFirst({
+            where: {
+                invoiceNumber: {
+                    startsWith: `INV${year}${month}`
+                },
             },
-        },
-        orderBy: {
-            invoiceNumber: "desc", // Facture avec le plus grand numéro
-        },
+            orderBy: {
+                invoiceNumber: "desc",
+            },
+        });
+
+        let nextNumber: number;
+        if (!lastInvoice) {
+            nextNumber = 1;
+        } else {
+            const lastSequence = parseInt(lastInvoice.invoiceNumber.slice(-6), 10);
+            nextNumber = lastSequence + 1;
+        }
+
+        return `INV${year}${month}${nextNumber.toString().padStart(6, '0')}`;
     });
-
-    let nextNumber : number; // Par défaut, commence à 1 si aucune facture pour l'année
-
-    if(!lastInvoice) {
-        nextNumber = 1;
-    } else {
-        // Extraire le dernier numéro et l'incrémenter
-        const lastSequence = parseInt(lastInvoice.invoiceNumber.slice(-6));
-        nextNumber = lastSequence + 1;
-    }
-
-    // Example:     inv 2401000001
-    const nextInvoiceNumber = `INV${year}${month}${nextNumber.toString().padStart(6, '0')}`;
-    return nextInvoiceNumber;
 }
