58a47ba80f03db4cf0437020e428167a750c079c
Faisal Bentouhami <bentouhami.faisal@gmail.com>
2025-01-23 23:50:06 +0100

    route api updated to generate invoice number

--- src/app/api/v1/invoices/generate-number/route.ts
+++ src/app/api/v1/invoices/generate-number/route.ts
@@ -1,28 +1,23 @@
 // path: src/app/api/v1/invoices/generate-number/route.ts
-export const dynamic = 'force-dynamic'
-
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { generateInvoiceNumber } from '@/lib/utils/invoice';
 
-export async function GET(req: NextResponse) {
+export async function GET(req: NextRequest) {
+    if (req.method !== 'GET') {
+        return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
+    }
+
     try {
         const invoiceNumber = await generateInvoiceNumber();
         if (!invoiceNumber) {
             return NextResponse.json({ message: 'Invoice number not found' }, { status: 404 });
         }
-
-        // Disable caching
-        const headers = new Headers();
-        headers.set('Cache-Control', 'no-store, max-age=0');
-        headers.set('Pragma', 'no-cache');
-        headers.set('Expires', '0');
-
-        return NextResponse.json({ invoiceNumber }, { headers });
+        return NextResponse.json({ invoiceNumber });
     } catch (error) {
         console.error('Error generating invoice number:', error);
         return NextResponse.json(
-            { message: 'Error generating invoice number' },
-            { status: 500 },
+            { message: "Error generating invoice number" },
+            { status: 500 }
         );
     }
 }
